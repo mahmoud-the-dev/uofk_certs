a08335c972267aaeaaf22589046d7be4081695f3
mahmoud-the-dev <doon1236@gmail.com>
2022-09-05 13:29:22 +0200

    000

--- UOFK_Certs/certificates/views.py
+++ UOFK_Certs/certificates/views.py
@@ -14,9 +14,10 @@ def universityNumberCheck(request):
     students = Student.objects.all().values()
     if request.method == 'POST':
         uni_num = request.POST.get('uni_num')
-        student1= Student.objects.filter(university_number=uni_num)
-        if (student1):
-            print('GET IT')
+        student= Student.objects.filter(university_number=uni_num).values()
+        if (student):
+            request.session['uni_num'] = int(uni_num)
+            return redirect('/new_request',uni_num=uni_num)
         else:
             print('NOT FOUND')
     context = {'students': students}
@@ -27,10 +28,30 @@ def trackCertReq(request):
     return render(request, 'certificates/track_cert_req.html')
 
 
-def createReq(request,uni_num):
+def createReq(request):
+    uni_num= request.session['uni_num']
+    student = Student.objects.get(pk=uni_num)
 
-    student = Student.objects.get(university_number=uni_num)
 
-    context= {'student':student}
+    completed_levels = student.completedlevel_set.all()
 
-    return render(request,'certificates/req_form.html')
+    levels = []
+
+    for level in completed_levels:
+        this_level=Level.objects.get(level=level.level)
+        types = this_level.leveltypelink_set.all()
+        this_types = []
+        for type in types:
+            this_type=CertificateType.objects.get(type=type.certificate_type)
+            languages = this_type.typelanguage_set.all()
+            this_languages = []
+            for language in languages:
+                this_language= language.language
+                this_languages.append(this_language)
+            dict1 =[this_type,this_languages]
+            this_types.append(dict1)
+        dict2 = [this_level,this_types]
+        levels.append(dict2)
+
+    context= {'levels':levels}
+    return render(request,'certificates/req_form.html',context=context)
