240b90b5f58f7ae7b59a7aec664c7c891b963b10
Mohammed-altahir <75260113+Mohammed-altahir@users.noreply.github.com>
2022-09-05 18:54:15 +0200

    Create urls.py

--- UOFK_Certs/employee/urls.py
+++ UOFK_Certs/employee/urls.py
@@ -2,4 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('employee',views.home,name='employee home'),
+    path('employee/login',views.login,name='employee login'),
+    path('employee/verify',views.verify,name='employee verification'),
+    path('employee/menu',views.menu,name='employee menu'),
+    path('employee/certifcate-settings',views.certificate_settings,name='certificate settings'),
+    path('employee/certificate-requests',views.manage_certificate_requests,name='certificate requests'),
+    path('employee/add-certificate',views.add_certificate,name='add certificate'),
+    path('employee/update-certificate',views.update_certificate,name='update certificate'),
+
+
 ]
