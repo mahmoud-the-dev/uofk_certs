14525d0dbb680e76ab79899fd5b478ea3d5eaeb9
Mohammed-altahir <75260113+Mohammed-altahir@users.noreply.github.com>
2022-09-06 17:01:44 +0200

    Update urls.py

--- UOFK_Certs/employee/urls.py
+++ UOFK_Certs/employee/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('employee',views.home,name='employee home'),
-    path('employee/login',views.login,name='employee login'),
-    path('employee/verify',views.verify,name='employee verification'),
-    path('employee/menu',views.menu,name='employee menu'),
-    path('employee/certifcate-settings',views.certificate_settings,name='certificate settings'),
-    path('employee/certificate-requests',views.manage_certificate_requests,name='certificate requests'),
-    path('employee/add-certificate',views.add_certificate,name='add certificate'),
-    path('employee/update-certificate',views.update_certificate,name='update certificate'),
+    path('employee',home,name='employee home'),
+    path('employee/login',adlogin,name='employee login'),
+    path('employee/verify',verify,name='employee verification'),
+    path('employee/menu',menu,name='employee menu'),
+    path('employee/certifcate-settings',certificate_settings,name='certificate settings'),
+    path('employee/certificate-requests',ManageCertificateRequests.as_view,name='certificate requests'),
+    path('employee/view-request',ViewRequest.as_view(),name='view request'),
+    path('employee/add-certificate',add_certificate,name='add certificate'),
+    path('employee/update-certificate',update_certificate,name='update certificate'),
 
 
 ]
