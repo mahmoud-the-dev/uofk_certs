f5d92d0e3f91d18d7d4ee70c25b429bb2139af83
Mohammed-altahir <75260113+Mohammed-altahir@users.noreply.github.com>
2022-09-06 17:02:08 +0200

    Update views.py

--- UOFK_Certs/employee/views.py
+++ UOFK_Certs/employee/views.py
@@ -1,4 +1,14 @@
+from random import randint
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from django.views.generic import View
+from UOFK_Certs.certificates.models import CertificateItem, CertificateRequest
+
+
+from certificates.models import CertificateType
+
 
 
 def home(request):
@@ -6,14 +16,13 @@ def home(request):
 
 
 
-def login(request):
+def adlogin(request):
     return render(request, 'employee/p2.html')
-
+  
 
 def verify(request):
     return render(request, 'employee/p3.html')
-
-    
+  
 def menu(request):
     return render(request, 'employee/p4.html')
 
@@ -21,8 +30,22 @@ def menu(request):
 def certificate_settings(request):
     return render(request, 'employee/p5.html')
 
-def manage_certificate_requests(request):
-    return render(request, 'employee/p6.html')
+class ManageCertificateRequests(View):
+    requests = CertificateRequest.objects.get()
+    context = {
+        'object':requests
+    }
+    def get(self):
+        return render(self.request, 'employee/p6.html',self.context)
+
+
+class ViewRequest(View):
+    certs = CertificateItem.objects.get() 
+    context = {
+        'object': certs
+    }
+    def get(self):
+        return render(self.request, 'employee/p6_2.html',self.context)
 
 def add_certificate(request):
     return render(request, 'employee/p7.html')
